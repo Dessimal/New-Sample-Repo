9aaf54c7d0615892dfa9e8adef82b7ac16355fe2
odesanmimichael@gmail.com <odesanmimichael@gmail.com>
2023-03-22 13:43:18 +0100

    testing pushing

---
+++ Coded/another.js
@@ -0,0 +1,67 @@
+// const hAmb = document.querySelector(".hamb");
+// const navList = document.querySelector(".menu-list");
+// const naVIGATION = document.querySelector(".navigation");
+
+
+const openBtn = document.getElementById("btnone");
+
+
+const popUP = document.getElementById("popup");
+
+const bodyBlur = document.getElementById("blur");
+
+// const reDirect = document.getElementById("redirect");
+
+const form = document.getElementById("form");
+
+
+openBtn.onclick= function (){
+    popUP.classList.add("active");
+    bodyBlur.classList.add("active");
+}
+
+// closeBtn.onclick= function (){
+
+// // function closePopup(){
+//     popUP.classList.remove("active");
+//     bodyBlur.classList.remove("active");
+
+// }
+
+// form.addEventListener("submit", redir)
+
+// function redir(){
+//     redir.preventDefault;
+//     var firstname = getElementById("firstname").value;
+//     var phone = getElementById("phone").value;
+//     window.location.href = "https://wa.link/eqizd4"}
+
+
+// const  openPopup = document.getElementById("openpopup");
+// const closePopup = document.getElementById("click");
+// const makeBlur = document.getElementById("inner");
+// const popUP = document.getElementById("popup")
+
+//  openPopup.onclick = function(){
+//    makeBlur.classList.toggle("active");
+//    popUP.classList.toggle("active");
+   
+
+//  }
+//  closePopup.onclick = function(){
+//     makeBlur.classList.remove("active");
+//     popUP.classList.remove("active");
+//  }
+
+
+
+
+
+// hAmb.addEventListener("click", show);
+
+// function show() {
+//     navList.classList.toggle("active");
+//     hAmb.classList.toggle("active");
+//     naVIGATION.classList.toggle("active");
+// }
+
